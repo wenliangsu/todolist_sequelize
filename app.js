0f71d9610f5f93a70ae4bcce2debc59dd3cf532e
wenliangsu <wenliangsu65018@gmail.com>
2023-01-03 16:01:59 +0800

    Feat: add login and register and login page

--- app.js
+++ app.js
@@ -5,14 +5,37 @@ const bcrypt = require('bcryptjs');
 const app = express();
 const PORT = 3000;
 
+//Section middleware
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+//Section router
 app.get('/', (req, res) => {
   res.send('hello word!');
 });
 
+app.get('/users/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/users/login', (req, res) => {
+  res.send('login');
+});
+
+app.get('/users/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/users/register', (req, res) => {
+  res.send('register');
+});
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout');
+});
+
 app.listen(PORT, () => {
-  console.log(`App is runnung on http://localhost:${PORT}`);
+  console.log(`App is running on http://localhost:${PORT}`);
 });
