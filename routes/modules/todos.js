8de52981ba64f1752e7f0787557db91ca1425e33
wenliangsu <wenliangsu65018@gmail.com>
2023-01-10 14:26:20 +0800

    Feat: add the CRUD function of create

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,7 +4,20 @@ const router = express.Router();
 //note 在migrations跟models設定完後才可使用
 const db = require('../../models');
 const Todo = db.Todo;
+// todo create
+router.get('/new', (req, res) => {
+  return res.render('new');
+});
 
+router.post('/', (req, res) => {
+  const UserId = req.user.id;
+  const name = req.body.name;
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error));
+});
+
+//todo read
 // Thinking 原先未關聯之前使用findByPk(id)，如此只會找到todo list的id，但是不知道使用者是誰，所以透過findOne並搭配where語法，找出兩者的交集後，顯現資料
 router.get('/:id', (req, res) => {
   const id = req.params.id;
